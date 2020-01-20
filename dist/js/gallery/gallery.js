62af8983950205070a4a9aa25bc83474e0835f90
Victor Mitskevich <vmitskevich@Victors-Mac-mini.local>
2020-01-20 16:27:24 +0300

    refactor: add comments

--- dist/js/gallery/gallery.js
+++ dist/js/gallery/gallery.js
@@ -13,27 +13,34 @@ export default class Gallery {
     addClickEventListener() {
         this.container.addEventListener('click', (event) => {
             let targetElParentContainer;
+
+            // event path for differents browser:
             const eventPath = event.path || (event.composedPath && event.composedPath());
+
+            // check what button was pressed
             const pressedElement = checkTargetClassInPath(eventPath, event.currentTarget, 'item__text-button') || 
                                     checkTargetClassInPath(eventPath, event.currentTarget, 'item__info-button');
 
+            // if not button, return
             if (!pressedElement) {
                 return;
             }
 
+            // get an html element of list
             targetElParentContainer = pressedElement;
             while (!targetElParentContainer.classList.contains('section__gallery-item')) {
                 targetElParentContainer = targetElParentContainer.parentNode;
             }
 
+            // message element
             const targetElMessage = targetElParentContainer.querySelector('.item__info-wrapper');
 
+            // add/remove classes on message element
             if (pressedElement.classList.contains('item__text-button')) {
                 targetElMessage.classList.remove('_hidden');
                 targetElMessage.classList.remove('_transparent');
             } else if (pressedElement.classList.contains('item__info-button')) {
                 targetElMessage.classList.add('_transparent');
-                // targetElMessage.classList.add('_hidden');
             }
         });
     }
