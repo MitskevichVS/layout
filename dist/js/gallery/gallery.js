c9c420c33c65cd124dff7b099499634cc065b4d5
Victor Mitskevich <vmitskevich@Victors-Mac-mini.local>
2020-01-20 14:40:25 +0300

    refactor: gallery script

--- dist/js/gallery/gallery.js
+++ dist/js/gallery/gallery.js
@@ -11,14 +11,26 @@ export default class Gallery {
 
     addGalleryEventListener() {
         this.container.addEventListener('click', (event) => {
+            let targetElParentContainer;
             const eventPath = event.path || (event.composedPath && event.composedPath());
-            const pressedElement = checkTargetClassInPath(eventPath, event.currentTarget, 'item__text-button') || checkTargetClassInPath(eventPath, event.currentTarget, 'item__info-button');
-            if (!pressedElement) return;
-            const targetElParentContainer = pressedElement.parentNode.parentNode;
+            const pressedElement = checkTargetClassInPath(eventPath, event.currentTarget, 'item__text-button') || 
+                                    checkTargetClassInPath(eventPath, event.currentTarget, 'item__info-button');
+
+            if (!pressedElement) {
+                return;
+            }
+
+            targetElParentContainer = pressedElement;
+            while (!targetElParentContainer.classList.contains('section__gallery-item')) {
+                targetElParentContainer = targetElParentContainer.parentNode;
+            }
+
+            const targetElMessage = targetElParentContainer.querySelector('.item__info-wrapper');
+
             if (pressedElement.classList.contains('item__text-button')) {
-                targetElParentContainer.children[2].classList.remove('_transparent', '_hidden');
+                targetElMessage.classList.remove('_transparent', '_hidden');
             } else if (pressedElement.classList.contains('item__info-button')) {
-                targetElParentContainer.children[2].classList.add('_transparent', '_hidden');
+                targetElMessage.classList.add('_transparent', '_hidden');
             }
         });
     }
--- dist/js/utils/pathChecker.js
+++ dist/js/utils/pathChecker.js
@@ -2,7 +2,7 @@ const checkTargetClassInPath = (pathPseudoArray, eventEl, targetClass) => {
     let requiredNode = false;
     const pathArray = Array.from(pathPseudoArray);
     for (let itemIndex = 0; itemIndex < pathArray.length; itemIndex += 1) {
-        if (pathArray[itemIndex].classList !== undefined && pathArray[itemIndex].classList.contains(targetClass) && pathArray[itemIndex] !== eventEl){
+        if (pathArray[itemIndex].classList !== undefined && pathArray[itemIndex].classList.contains(targetClass) && pathArray[itemIndex] !== eventEl) {
             requiredNode = pathArray[itemIndex];
             break;
         }
