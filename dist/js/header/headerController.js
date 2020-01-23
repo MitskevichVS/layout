7c6dfd2b77ba04eb02b030367f0eb697b2b55a74
Victor Mitskevich <vmitskevich@Victors-Mac-mini.local>
2020-01-23 17:23:15 +0300

    feat: close menu by outside click

--- dist/js/header/headerController.js
+++ dist/js/header/headerController.js
@@ -1,3 +1,5 @@
+import checkTargetClassInPath from '../utils/pathChecker.js';
+
 export default class HeaderController {
     constructor(headerMenuContainer, headerHeading) {
         this.headerMenuContainer = headerMenuContainer;
@@ -25,9 +27,27 @@ export default class HeaderController {
 
     headerMenuButtonEventListener() {
         const button = document.querySelector('.header__menu-button');
+        const animatedHeaderMenu = document.querySelector('.header__menu');
         button.addEventListener('click', () => {
             button.classList.toggle('_active');
             button.classList.toggle('_not-active');
-        })
+            animatedHeaderMenu.classList.toggle('_active');
+            animatedHeaderMenu.classList.toggle('_not-active');
+        });
+        document.addEventListener('click', (event) => {
+            const targetContainer = event.currentTarget;
+            const eventPath = event.path || (event.composedPath && event.composedPath());
+            const menuButton = checkTargetClassInPath(eventPath, targetContainer,'header__menu-button');
+            const menuBody = checkTargetClassInPath(eventPath, targetContainer, 'header__menu');
+
+            if (!menuButton && !menuBody) {
+                button.classList.remove('_active');
+                animatedHeaderMenu.classList.remove('_active');
+
+                button.classList.add('_not-active');
+                animatedHeaderMenu.classList.add('_not-active');
+            }
+
+        });
     }
 }
