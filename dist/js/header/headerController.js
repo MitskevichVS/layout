4c11b3b4442a189380672d2bf1478e6a08261d8d
Victor Mitskevich <vmitskevich@Victors-Mac-mini.local>
2020-01-23 14:50:43 +0300

    fix: add safe interval/ debounce for safari

--- dist/js/header/headerController.js
+++ dist/js/header/headerController.js
@@ -2,11 +2,14 @@ export default class HeaderController {
     constructor(headerMenuContainer, headerHeading) {
         this.headerMenuContainer = headerMenuContainer;
         this.headerHeading = headerHeading;
-        this.previousCoordinate = window.pageYOffset;
+        this.previousCoordinate = 0;
+        this.scrollUpFlag = false;
     }
 
     init() {
         this.pageScrollListener();
+        //set timer to update scroll coordinate
+        setInterval(this.checkPreviousCoordinate.bind(this), 300);
     }
 
     pageScrollListener() {
@@ -14,23 +17,36 @@ export default class HeaderController {
     }
 
     scrollEvent() {
+        const scrollValueWithSafeInterval = document.documentElement.scrollHeight - document.documentElement.clientHeight - 150;
+        const scrollBounce = scrollValueWithSafeInterval <= document.documentElement.scrollTop;
 
-        if (document.documentElement.scrollHeight - document.documentElement.scrollTop === document.documentElement.clientHeight) {
+        // Check bounce (for Safari)
+        if (scrollBounce) {
             return;
         }
 
-        if (this.previousCoordinate < pageYOffset && pageYOffset >= 100) {
+        // Checking conditions for dynamic menu display
+        if (this.previousCoordinate - pageYOffset >= 0 && this.scrollUpFlag === true) {
+            this.headerMenuContainer.classList.remove('_hidden');
+            this.scrollUpFlag = true;
+        } else if (this.previousCoordinate <= pageYOffset + 70 && pageYOffset >= 100) {
             this.headerMenuContainer.classList.add('_hidden');
+            this.scrollUpFlag = false;
         } else {
             this.headerMenuContainer.classList.remove('_hidden');
+            this.scrollUpFlag = true;
         }
 
+        // Checking conditions to fix the menu
         if (pageYOffset >= 118) {
             this.headerMenuContainer.classList.add('_stick');
         } else {
             this.headerMenuContainer.classList.remove('_stick');
         }
-
-        this.previousCoordinate = pageYOffset;
     }
+
+    checkPreviousCoordinate() {
+        // update coordinate
+        this.previousCoordinate = pageYOffset;
+    };
 }
