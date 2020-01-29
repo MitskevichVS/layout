fef603c37ad344e82bf544c34b73d78e2f7257b7
Victor Mitskevich <vmitskevich@Victors-Mac-mini.local>
2020-01-29 12:27:49 +0300

    fix: array destructuring

--- dist/js/slider/slider.js
+++ dist/js/slider/slider.js
@@ -2,8 +2,7 @@ import addSliderEventListeners from '../utils/sliderEventListeners.js';
 
 export default class Slider {
   constructor(element) {
-    [this.sliderControlsNode] = element.children;
-    [, this.sliderContentNode] = element.children;
+    [this.sliderControlsNode, this.sliderContentNode] = element.children;
     this.sliderControls = Array.from(this.sliderControlsNode.children);
     this.sliderContent = Array.from(this.sliderContentNode.children);
   }
