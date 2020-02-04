13772122815aa186b4a4f30cdfa1d37a8031caca
Victor Mitskevich <vmitskevich@Victors-Mac-mini.local>
2020-02-04 15:41:24 +0300

    fix: onload function and fetch call

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,24 +1,26 @@
 import Slider from './slider/slider.js';
-import Gallery from './gallery/gallery.js';
+import InteractiveGallery from './gallery/interactiveGallery.js';
+import StaticGallery from './gallery/staticGallery.js';
 import HeaderController from './header/headerController.js';
 import QueriesController from './queries/queries.js';
 
 window.onload = () => {
   const slidersArray = Array.from(document.querySelectorAll('.section__content-slider'));
-  const [staticGallery, dynamicGallery] = document.querySelectorAll('.section__gallery-list');
+  const [staticGallery, interactiveGallery] = document.querySelectorAll('.section__gallery-list');
   const headerMenuContainer = document.querySelector('.header__content-top');
   const headerHeading = document.querySelector('.header__heading');
 
-  const galleryClass = new Gallery(staticGallery, dynamicGallery);
+  const interactGalleryClass = new InteractiveGallery(interactiveGallery);
+  const staticGalClass = new StaticGallery(staticGallery);
   const headerClass = new HeaderController(headerMenuContainer, headerHeading);
-  const queriesClass = new QueriesController(galleryClass);
+  const queriesClass = new QueriesController();
 
   slidersArray.forEach((slider) => {
     const sliderClass = new Slider(slider);
     sliderClass.init();
   });
 
-  galleryClass.init();
+  interactGalleryClass.init();
 
   headerClass.init();
 
@@ -26,7 +28,9 @@ window.onload = () => {
     headerClass.handleScrollEvent();
   }
 
-  queriesClass.init();
+  queriesClass.getJSONfromImageService().then((data) => {
+    staticGalClass.fillStaticGallery(data);
+  });
 
   setTimeout(() => document.querySelector('.hide').classList.remove('hide'), 250);
 };
