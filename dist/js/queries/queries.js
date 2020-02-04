001b117599997a8b90c2edfe6f5b5c01774c6eb9
Victor Mitskevich <vmitskevich@Victors-Mac-mini.local>
2020-02-04 15:39:32 +0300

    fix: fetch method

--- dist/js/queries/queries.js
+++ dist/js/queries/queries.js
@@ -1,21 +1,19 @@
 export default class QueriesController {
-  constructor(galleryClass) {
-    this.galleryClass = galleryClass;
+  constructor() {
     this.imgApiUrl = 'https://api.unsplash.com/search/photos?';
     this.imgApiKey = '006c60908ccf1e3d0cc97d6a3cccb8bc6ca0ad21be33e45275da97139738ac0f';
     this.imgItemsPerPage = 6;
     this.imgResponsePage = Math.floor(Math.random() * 50) + 1; // random page
     this.receivedData = null;
+    // flickr url:
+    // https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=41f8896bf49f34b7c4a7ba3dc7fbbd06&tags=minimal&per_page=6&format=json&nojsoncallback=1
+    // get photos from flickr: https://www.flickr.com/services/api/misc.urls.html
   }
 
-  init() {
-    this.getJSONfromImageService();
-  }
-
-  async getJSONfromImageService() {
+  getJSONfromImageService() {
     const url = `${this.imgApiUrl}query=minimal&client_id=${this.imgApiKey}&per_page=${this.imgItemsPerPage}&page=${this.imgResponsePage}`;
 
-    await fetch(url,
+    return fetch(url,
       {
         method: 'GET',
         headers: {
@@ -26,8 +24,7 @@ export default class QueriesController {
       .then((response) => response.json())
       .then((receivedData) => {
         const { results } = receivedData;
-        this.receivedData = results;
-        this.galleryClass.fillStaticGallery(this.receivedData);
+        return results;
       })
       .catch((err) => {
         // log error or show error screen
